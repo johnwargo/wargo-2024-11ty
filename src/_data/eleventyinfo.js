e731c32c144671c0a7eb263418a821c322f8c435
johnwargo <john@johnwargo.com>
2024-12-29 09:46:00 -0500

    implemented generator tag and footer

--- src/_data/eleventyinfo.js
+++ src/_data/eleventyinfo.js
@@ -1,7 +1,15 @@
-'use strict'
+// 'use strict'
 
-const eleventyPackage = require('@11ty/eleventy/package.json')
+// const eleventyPackage = require('@11ty/eleventy/package.json')
+
+// module.exports = function () {
+//   return { generatorStr: `${eleventyPackage.name} v${eleventyPackage.version}` }
+// }
+
+
+const fs = require('fs');
 
 module.exports = function () {
-  return { generatorStr: `${eleventyPackage.name} v${eleventyPackage.version}` }
+  const packageJson = JSON.parse(fs.readFileSync('./node_modules/@11ty/eleventy/package.json', 'utf8'));
+  return { generatorStr: `${packageJson.name} v${packageJson.version}` }
 }
